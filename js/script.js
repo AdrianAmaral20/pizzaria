d39bcada954149c0661af5cb25aa11efef137dcf
Adrian Amaral <adri13amaral@gmail.com>
2022-11-11 12:14:39 -0300

    concluido as ações no modal, quantidade, tamanho e botão de fechar funcionando

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let modalQt = 1
 const c = (e) => document.querySelector(e);
 const cs = (e) => document.querySelectorAll(e);
 
+
+// Listagem das pizzas
 pizzaJson.map((e, i) => {
   let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
@@ -38,4 +40,36 @@ pizzaJson.map((e, i) => {
 
 
   c('.pizza-area').append(pizzaItem);
+});
+
+
+// Eventos do Modal
+function closeModal() {
+  c('.pizzaWindowArea').style.opacity = 0;
+  setTimeout(() => {
+    c('.pizzaWindowArea').style.display = 'none';
+  }, 500);
+};
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((i) => {
+  i.addEventListener('click', closeModal)
+});
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+  if (modalQt > 1) {
+    modalQt--;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+  }
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+  modalQt++;
+  c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+  size.addEventListener('click', (e) => {
+    c('.pizzaInfo--size.selected').classList.remove('selected');
+    size.classList.add('selected');
+  });
 });
